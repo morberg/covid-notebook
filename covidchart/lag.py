1db61de8fec50f160e04d795cfc56441a27e7b40
Niklas Morberg <niklas.morberg@gmail.com>
2020-05-08 14:45:17 +0200

    Put chart generation in package

---
+++ covidchart/lag.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import altair as alt
+
+def lag_chart_with_selection(df, labels):
+    """
+    Generate chart showing lag in reporting with interactive filter on reported date
+
+    Input
+    -----
+    df : pandas.DataFrame
+        Typically generated from get_fhm_data() in coviddata package
+    labels : array
+        Labels for column 'lag' in data. Returned from get_fhm_data()
+
+    Returns
+    -------
+    chart : alt.Chart
+    """
+
+    # Dates to display on the x-axis
+    domain = [df.date.min().date().isoformat(), (df.date.max() + pd.Timedelta('1D')).date().isoformat()]
+
+    brush = alt.selection(type='interval', encodings=['x'])
+
+    deceased = alt.Chart(df, height=400, width=800).mark_bar().encode(
+        x=alt.X('yearmonthdate(date)', title='Date Deceased', scale=alt.Scale(domain=domain)),
+        y=alt.Y('sum(n_diff)', title='Deceased', scale=alt.Scale(domain=[0, df.N.max()])),
+        order=alt.Order(
+        # Sort the segments of the bars by this field
+        'days_since_publication',
+        sort='ascending'
+        ),
+        color=alt.Color(
+            'lag:O',
+            title='Lag in Days',
+            sort=labels,
+            scale=alt.Scale(scheme='category20c'),
+        )
+    ).transform_filter(
+        brush
+    )
+
+    reported = alt.Chart(df, height=100, width=800).mark_bar().encode(
+        x=alt.X('yearmonthdate(publication_date)', title='Publication Date', scale=alt.Scale(domain=domain)),
+        y=alt.Y('sum(n_diff)', title='Reported Deaths')
+    ).add_selection(brush)
+
+    chart = (deceased & reported)
+    return chart
