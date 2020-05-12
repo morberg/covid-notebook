78d58d77b33246cc2c42696034a175f66c4b9fa6
Niklas Morberg <niklas.morberg@gmail.com>
2020-05-12 19:54:56 +0200

    Refactor

--- covid/dataimport.py
+++ covid/dataimport.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import locale
 
 def get_fhm_data():
     """
@@ -21,3 +22,33 @@ def get_fhm_data():
     data['lag'] = pd.cut(data['days_since_publication'], bins, labels=labels)
     data['age'] = data.days_since_publication * data.n_diff
     return data, labels
+
+def get_scb_county_data():
+    """
+    Download data published weekly by Statistiska Centralbyrån
+
+    Returns
+    -------
+    data : pandas.DataFrame
+        Data containing all deaths in Sweden 2018-2020 by county
+    """
+
+    # Needed to parse date strings from SCB
+    locale.setlocale(locale.LC_ALL, 'sv_SE'
+    ) 
+    location="https://scb.se/hitta-statistik/statistik-efter-amne/befolkning/befolkningens-sammansattning/befolkningsstatistik/pong/tabell-och-diagram/preliminar-statistik-over-doda/"
+
+    rows_to_drop = [0, 1]
+    data = pd.read_excel(location, sheet_name="Tabell 3", skiprows=5, header=1, usecols='A:W', na_values="..")
+    data['date'] = pd.to_datetime(data['Unnamed: 0'] + data['Year'].apply(str), format='%d %B%Y', errors='coerce')
+    data.drop(columns=['Unnamed: 0', 'Year'], inplace=True)
+    data.drop(rows_to_drop, inplace=True)
+    data = data[:-3] # The 3 last rows are a sum of all values
+
+    rename_columns = {'Söderman-':'Södermanland', 'Öster-': 'Östergötland', 'Västra': 'Västra Götaland', 'Västman-': 'Västmanland', 'Västernorr-': 'Västernorrland', 'Väster-': 'Västerbotten', 'Norr-': 'Norrbotten'}
+    data.rename(columns=rename_columns, inplace=True)
+
+    data = data.melt('date', var_name='county', value_name='deaths')
+    data.deaths = data.deaths.fillna(0).astype(int)
+
+    return data
--- generate-html-charts.py
+++ generate-html-charts.py
@@ -1,10 +1,10 @@
 import altair as alt
 import pandas as pd
-from coviddata.fhm import get_fhm_data
-from covidchart.lag import lag_chart_with_selection
+from covid import dataimport
+from covid import chart
 
-fhm_data, labels = get_fhm_data()
+fhm_data, labels = dataimport.get_fhm_data()
 df = fhm_data[fhm_data['date'] >= '2020-03-13']
 
-chart = lag_chart_with_selection(df, labels)
+chart = chart.lag_chart_with_selection(df, labels)
 chart.save('charts/filter-publication-date.html')
